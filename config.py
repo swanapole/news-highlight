731d0377208fdef8cb02b60d9ecda78b3c8cfc19
swanapole <swanapoleshilla@yahoo.com>
2019-02-06 13:13:42 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     SOURCES_BASE_URL =" https://newsapi.org/v2/sources?language=en&apiKey=0f4ee6fd99a24df3826d0f34d3c08dab"
     EVERYTHING_BASE_URL="https://newsapi.org/v2/everything?sources={}s&apiKey=0f4ee6fd99a24df3826d0f34d3c08dab"
-
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
 class ProdConfig(Config):
@@ -26,3 +28,9 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
